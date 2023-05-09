aa56fa65b5d3e590c6055253b8dab4408b0502c1
DASitby <davidsooley551@gmail.com>
2023-05-09 17:01:46 +0000

    add new, create, and user_params to users controller)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = user.new(user_params)
+
+    # store all emails in lowercase to avoid duplicates and case-sensitivity
+    @user.email.downcase!
+
+    if @user.save
+      flash[:notice] = "Account created successfully!"
+      redirect_to root_path
+    else
+      flash.now.alert = "Please use a valid email"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmationits)
+  end
 end
