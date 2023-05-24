2fbfa7b1fa81f3017daed0bac3b0e3de49348f7d
DASitby <davidsooley551@gmail.com>
2023-05-24 05:02:54 +0000

    add case sensitive:false and password confirmation validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
-
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_confirmation, presence: true, on: :create
 end
