4688b3048ec1324322fd8ea95781111b4bdf78ee
DASitby <davidsooley551@gmail.com>
2023-05-24 03:32:01 +0000

    update tests with all parameters

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,23 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    let(:category) { Category.create(name: 'Example Category') }
+    
     it 'validates the presence of name' do
-      product = Product.new(name: nil)
+      product = Product.new(name: nil, price_cents: 999, quantity: 1, category: category)
       expect(product).not_to be_valid
       expect(product.errors[:name]).to include("can't be blank")
     end
      it 'validates the presence of price' do
-      product = Product.new(price_cents: nil)
+      product = Product.new(name: "David Sooley", price_cents: nil, quantity: 1, category: category)
       expect(product).not_to be_valid
       expect(product.errors[:price_cents]).to include("is not a number")
     end
     it 'validates the presence of quantity' do
-      product = Product.new(quantity: nil)
+      product = Product.new(name: "David Sooley", price_cents: 999, quantity: nil, category: category)
       expect(product).not_to be_valid
       expect(product.errors[:quantity]).to include("can't be blank")
     end
     it 'validates the presence of category' do
-      product = Product.new(category: nil)
+      product = Product.new(name: "David Sooley", price_cents: 999, quantity: 1, category: nil)
       expect(product).not_to be_valid
       expect(product.errors[:category]).to include("can't be blank")
     end
